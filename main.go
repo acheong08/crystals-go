8d59a32657461e75b690853f7786cc7845138e5e
Antonio Cheong <acheong@student.dalat.org>
2022-11-14 01:26:08 +0800

    Testing

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/acheong08/crystals-go/dilithium"
+)
+
+// Just testing a few things to make sure the code is working
+func main() {
+	d := dilithium.NewDilithium3()
+	pk, sk := d.KeyGen([]byte("test"))
+	msg := []byte("hello world")
+	sig := d.Sign(sk, msg)
+	if !d.Verify(pk, msg, sig) {
+		panic("failed")
+	} else {
+		println("success")
+	}
+}
