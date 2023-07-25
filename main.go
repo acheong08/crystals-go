4de850b26892971b2efffdf658135d4707e5b6cb
Antonio <acheong@student.dalat.org>
2023-07-25 16:39:29 +0800

    kyber test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/acheong08/crystals-go/dilithium"
+	"github.com/acheong08/crystals-go/kyber"
 )
 
 // Just testing a few things to make sure the code is working
@@ -11,6 +12,17 @@ func main() {
 	msg := []byte("hello world")
 	sig := d.Sign(sk, msg)
 	if !d.Verify(pk, msg, sig) {
+		panic("failed")
+	} else {
+		println("success")
+	}
+	k := kyber.NewKyber512()
+	pk, sk = k.KeyGen([]byte("test"))
+	msg = []byte("hello world")
+	c, ss := k.Encaps(pk, msg)
+	newss := k.Decaps(sk, c)
+	// Convert to string and compare
+	if string(ss) != string(newss) {
 		panic("failed")
 	} else {
 		println("success")
